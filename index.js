a0f8a5cf050f3beeba54df72ef81787394fe1d03
sithmel <maurizio.lupo@gmail.com>
2018-11-25 03:27:10 +0000

    bump async-define, removed nom-release

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-var path = require('path');
-var fs = require('fs');
-var adPath = require.resolve('async-define');
-var adTemplatePath = path.join(adPath, '../../templates/main.hbs');
-var adTemplateFragments = fs.readFileSync(adTemplatePath, 'utf8').split('__placeholder__');
-var header = adTemplateFragments[0];
-var footer = adTemplateFragments[1];
+var path = require('path')
+var fs = require('fs')
+var adPath = require.resolve('async-define')
+var adTemplatePath = path.join(adPath, '../../templates/main.hbs')
+var adTemplateFragments = fs.readFileSync(adTemplatePath, 'utf8').split('__placeholder__')
+var header = adTemplateFragments[0]
+var footer = adTemplateFragments[1]
 
 module.exports = function () {
   return {
@@ -12,8 +12,8 @@ module.exports = function () {
     banner: header,
     footer: footer,
     options: function (opts) {
-      opts.format = 'amd';
-      return opts;
+      opts.format = 'amd'
+      return opts
     }
-  };
-};
+  }
+}
